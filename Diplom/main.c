bbd7d19e28aacd93b6bd911f1efc9f1a65812505
Alex Tsiganov <admin@Mac-Admin-2.local>
2015-03-17 15:57:18 +0300

    Initial Commit

---
+++ Diplom/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Diplom
+//
+//  Created by Alex Tsiganov on 17.03.15.
+//  Copyright (c) 2015 Alex Tsiganov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
