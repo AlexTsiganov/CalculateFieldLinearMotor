f81d90401a2c55c08b2afdf34c80d3aad130d703
Alex Tsiganov <alextsiganov@Mac-Admin.local>
2015-04-21 12:54:47 +0300

    created pdf folder

--- Scripts/Script.sh
+++ Scripts/Script.sh
@@ -6,4 +6,4 @@
 #  Created by Alex Tsiganov on 16.04.15.
 #  Copyright (c) 2015 Alex Tsiganov. All rights reserved.
 
-pdflatex /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Latex/1.tex && open /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Scripts/1.pdf
+pdflatex /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Latex/1.tex && mv /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Scripts/1.pdf /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Pdf/Inductor_model.pdf && open /Users/alextsiganov/Documents/University/Projects/CalculateFieldLinearMotor/Pdf/1.pdf
